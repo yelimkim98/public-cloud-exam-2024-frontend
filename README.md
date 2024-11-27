6ded1101f63fc11f1165d3896abbf1a70272d3cc
yelimkim98 <kiel0103@naver.com>
2024-11-27 18:52:32 +0900

    feat: users api response 출력

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Public Cloud Exam Frontend
+## local 실행 가이드
+```bash
+npm start
+```
--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Axios GET 요청 함수
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/users");
+        console.log(response)
+        setData(response.data); // 응답 데이터 설정
+      } catch (err) {
+        setError(err); // 에러 처리
+      } finally {
+        setLoading(false); // 로딩 상태 종료
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Data from API</h1>
+      {data && (
+        <ul>
+          {data.map((user) => (
+            <li key={user.id}>
+              ID: {user.id}, Name: {user.name}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
